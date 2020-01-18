db2b45618c70f4fad2b153ce77f329840db96f9f
Wojciech Opulski <wojtekopulski@gmail.com>
2020-01-18 22:12:25 +0100

    GoogleAuth reducer - user id added

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+class StreamList extends React.Component {
+  renderInput({ input, label, meta }) {
+    return(
+      <div className="field">
+        <label htmlFor="">{label}</label>
+        <input {...input}/>
+      </div>
+    )
+  }
+
+  onSubmit() {
+      
+  }
+
+  render() {
+    return <div>Stream List</div>;
+  }
+}
 
 export default StreamList;
