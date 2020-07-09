c00b5d4920063a67567f08b3a73b3dd48216ea2a
Wojciech Opulski <wojtekopulski@gmail.com>
2020-07-09 18:09:54 +0200

    added all action types for rest, changed Streams components, simple db added

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -19,9 +21,8 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = formValues =>  this.props.createStream(formValues);
+  
 
   render() {
     console.log(this.props);
@@ -56,7 +57,9 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(null, createStream)(formWrapped);
