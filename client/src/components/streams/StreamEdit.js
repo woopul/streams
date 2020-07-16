a75c53226519d6559ccce5505b13453afc0cac59
Wojciech Opulski <wojtekopulski@gmail.com>
2020-07-16 12:53:07 +0200

    Stream List updates, StreamEdit - added fetch stream & useEffect

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { arrayInsert } from "redux-form";
+import { connect } from "react-redux";
+import { fetchStream } from '../../actions/index'
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+const StreamEdit = (props) => {
+  console.log(props);
+
+  useEffect(() => {
+     setTimeout(() => {
+       props.fetchStream(props.match.params.id)
+      }, 2000);
+  }, []);
+
+  return (
+    <>
+      <div>StreamEdit</div>
+      <p>showing stream {props.match.params.id}, TITLE: </p>
+      {props.stream ? <p>{props.stream.title}</p> : <p>LOADING...</p>}
+    </>
+  );
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
