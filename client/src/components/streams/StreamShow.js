a7fcf32105d4042e9984e0b2395d73f3c00e7f35
Wojciech Opulski <wojtekopulski@gmail.com>
2020-07-22 00:44:50 +0200

    Streaming service added

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,20 +1,56 @@
-import React from "react";
-import { connect } from 'react-redux';
-import { fetchStream } from '../../actions'
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import flv from "flv.js";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
 const StreamShow = (props) => {
-  useEffect(() => {
-    props.fetchStream(props.match.params.id)
-    console.log('STREAM SHOW PROPS', props)
-  }, [])
+  const [player, setPlayer] = useState(undefined);
+  const [unmountCount, setUnmountCount] = useState(0);
+  const videoRef = React.createRef();
+  const { id } = props.match.params;
 
-  if(!props.stream) {
-    return (<div>LOADING...</div>)
+  useEffect(() => {
+    props.fetchStream(id);
+    buildPlayer();
+    loadPlayer();
+  }, []);
+  
+  // useEffect(() => {
+  //   buildPlayer();
+  //   loadPlayer();
+  // }, [player, props.stream]);
+  
+  // useEffect(() => {
+  //   setUnmountCount(unmountCount+1);
+  //   console.log("PLAYER, mountCount", player, unmountCount)
+  //   loadPlayer()
+  //   if(player) return () => {
+  //     console.log("I WAS UNMOUNTED");
+  //     player.destroy();
+  // //   };
+  // }, [player]);
+
+  const loadPlayer = () => {
+    if(!player) return;
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  }
+  
+  const buildPlayer = () => {
+    if(!props.stream) return ;
+    setPlayer(flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    }));
+  }
+
+  if (!props.stream) {
+    return <div>LOADING...</div>;
   }
   const { title, description } = props.stream;
   return (
     <div>
+      <video ref={videoRef} style={{ width: '100%' }} controls />
       <h1>{title}</h1>
       <h5>{description}</h5>
     </div>
@@ -22,7 +58,7 @@ const StreamShow = (props) => {
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  stream: state.streams[ownProps.match.params.id]
-})
+  stream: state.streams[ownProps.match.params.id],
+});
 
-export default connect(mapStateToProps, { fetchStream})(StreamShow);
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
