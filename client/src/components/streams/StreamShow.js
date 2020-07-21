63f09a4139510c6a89f51bd423abc5bfc0cd0e5d
Wojciech Opulski <wojtekopulski@gmail.com>
2020-07-21 18:06:53 +0200

    Stream show component refactored - showing title and description

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions'
+import { useEffect } from "react";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+const StreamShow = (props) => {
+  useEffect(() => {
+    props.fetchStream(props.match.params.id)
+    console.log('STREAM SHOW PROPS', props)
+  }, [])
+
+  if(!props.stream) {
+    return (<div>LOADING...</div>)
+  }
+  const { title, description } = props.stream;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => ({
+  stream: state.streams[ownProps.match.params.id]
+})
+
+export default connect(mapStateToProps, { fetchStream})(StreamShow);
